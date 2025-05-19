949b427772f63f770dd0ffe17efc178fd8807774
Martin Klomp <m.klomp@tudelft.nl>
2025-05-19 09:18:28 +0200

    Fixed python styling issues.

--- launch/gazebo_mirte_master_navigation.launch.py
+++ launch/gazebo_mirte_master_navigation.launch.py
@@ -24,54 +24,54 @@ from launch.actions import SetEnvironmentVariable
 
 
 def generate_launch_description():
-    pkg_robocup_home_simulation = get_package_share_directory(
-        'robocup_home_simulation')
+    pkg_robocup_home_simulation = get_package_share_directory("robocup_home_simulation")
     robocup_home_world_path = os.path.join(
-        pkg_robocup_home_simulation,
-        'worlds',
-        'KRR_Course_Small_house.world')
-    
-    pkg_mirte_gazebo = get_package_share_directory(
-        'mirte_gazebo')
+        pkg_robocup_home_simulation, "worlds", "KRR_Course_Small_house.world"
+    )
+
+    pkg_mirte_gazebo = get_package_share_directory("mirte_gazebo")
     mirte_gazebo_launch_path = os.path.join(
-        pkg_mirte_gazebo,
-        'launch',
-        'gazebo_mirte_master_empty.launch.xml')
+        pkg_mirte_gazebo, "launch", "gazebo_mirte_master_empty.launch.xml"
+    )
     mirte_gazebo_launch = IncludeLaunchDescription(
         XMLLaunchDescriptionSource(mirte_gazebo_launch_path),
         launch_arguments={
-            'world': robocup_home_world_path,
-        }.items()
+            "world": robocup_home_world_path,
+        }.items(),
     )
 
-    pkg_mirte_navigation = get_package_share_directory(
-        'mirte_navigation')
+    pkg_mirte_navigation = get_package_share_directory("mirte_navigation")
     mirte_navigation_launch_path = os.path.join(
-        pkg_mirte_navigation,
-        'launch',
-        'robot_navigation.launch.py')
+        pkg_mirte_navigation, "launch", "robot_navigation.launch.py"
+    )
 
     mirte_navigation_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(mirte_navigation_launch_path)
     )
-    
-    pkg_plasys_house_world = get_package_share_directory(
-        'plasys_house_world')
-    plasys_house_world_models = os.path.join(
-        pkg_plasys_house_world,
-        'models')
+
+    pkg_plasys_house_world = get_package_share_directory("plasys_house_world")
+    plasys_house_world_models = os.path.join(pkg_plasys_house_world, "models")
 
     pkg_aws_robomaker_small_house_world = get_package_share_directory(
-        'aws_robomaker_small_house_world')
+        "aws_robomaker_small_house_world"
+    )
     aws_robomaker_small_house_world_models = os.path.join(
-        pkg_aws_robomaker_small_house_world,
-        'models')
+        pkg_aws_robomaker_small_house_world, "models"
+    )
 
-    return LaunchDescription([
-        SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value=[
-            EnvironmentVariable('GAZEBO_MODEL_PATH'),
-            ':',plasys_house_world_models,
-            ':',aws_robomaker_small_house_world_models]),
-        mirte_navigation_launch,
-        mirte_gazebo_launch,
-    ])
+    return LaunchDescription(
+        [
+            SetEnvironmentVariable(
+                name="GAZEBO_MODEL_PATH",
+                value=[
+                    EnvironmentVariable("GAZEBO_MODEL_PATH"),
+                    ":",
+                    plasys_house_world_models,
+                    ":",
+                    aws_robomaker_small_house_world_models,
+                ],
+            ),
+            mirte_navigation_launch,
+            mirte_gazebo_launch,
+        ]
+    )
